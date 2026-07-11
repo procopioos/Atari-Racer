8868922c96301d852c51598c1576355e3a19041a
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2026-07-11 17:26:19 +0100

    there

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,6 @@ import pygame
 @pytest.fixture(scope="session", autouse=True)
 def pygame_initialize():
     pygame.init()
-    # some draw helpers need an active display
     pygame.display.set_mode((800, 600))
     yield
     pygame.quit()
